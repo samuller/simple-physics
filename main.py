16c7b03fe54a1f2c70b1427ba861d2da5cfecf8e
Simon Muller <samullers@gmail.com>
2019-04-22 23:13:32 +0200

    Add physics object class and update function

--- main.py
+++ main.py
@@ -14,38 +14,51 @@ SCROLL_DOWN = 5
 
 BALL_COLOR = (255, 128, 0)
 BALL_RADIUS = 10
-balls = [(90, 90)]
+
+
+class PhysicsObject:
+
+    def __init__(self, pos=(0,0), spd=(0,0)):
+        self.pos = pos
+        self.spd = spd
+
+
+objects = [PhysicsObject((90, 90), (+1, +1))]
 
 
 def draw(screen):
     color = (32, 64, 96)
     pygame.draw.rect(screen, color, pygame.Rect(0, 0, WIDTH, HEIGHT))
 
-    for ball_pos in balls:
-        pygame.draw.circle(screen, BALL_COLOR, ball_pos, BALL_RADIUS)
+    for object in objects:
+        pygame.draw.circle(screen, BALL_COLOR, object.pos, BALL_RADIUS)
 
 
 def handle_event(event):
     if event.type == pygame.MOUSEBUTTONDOWN:
         if event.button == LEFT_CLICK:
             pos = pygame.mouse.get_pos()
-            balls.append(pos)
+            objects.append(PhysicsObject(pos))
         if event.button == RIGHT_CLICK:
             pos = pygame.mouse.get_pos()
             remove_el = None
-            for ball in balls:
-                diff = numpy.subtract(pos, ball)
+            for obj in objects:
+                diff = numpy.subtract(pos, obj.pos)
                 if numpy.linalg.norm(diff) <= BALL_RADIUS:
-                    remove_el = ball
+                    remove_el = obj
                     break
             if remove_el is not None:
-                balls.remove(remove_el)
+                objects.remove(remove_el)
 
 
-def draw_fps(screen, clock, font):
-    fps = int(clock.get_fps())
+def draw_fps(screen, fps, font):
     fps_surface = font.render(str(fps), True, pygame.Color('white'))
     screen.blit(fps_surface, (0, 0))
+
+
+def update_physics(time_diff_ms):
+    for obj in objects:
+        obj.pos = numpy.add(obj.pos, obj.spd)
 
 
 def main_loop():
@@ -57,16 +70,18 @@ def main_loop():
 
     done = False
     while not done:
+        dt_ms = clock.tick(FRAMERATE_LIMIT)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
                 break
             handle_event(event)
+        update_physics(dt_ms)
 
         draw(screen)
-        draw_fps(screen, clock, font)
+        draw_fps(screen, int(clock.get_fps()), font)
         pygame.display.flip()
-        clock.tick(FRAMERATE_LIMIT)
 
     pygame.quit()
     sys.exit()
