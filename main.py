7356f8d20515c18a7682dbe3927fe93ddfa7e607
Simon Muller <samullers@gmail.com>
2019-04-22 21:55:50 +0200

    Simple visualization to add and remove balls

---
+++ main.py
@@ -0,0 +1,61 @@
+import numpy
+import pygame
+
+WIDTH = 640
+HEIGHT = 480
+
+LEFT_CLICK = 1
+MIDDLE_CLICK = 2
+RIGHT_CLICK = 3
+SCROLL_UP = 4
+SCROLL_DOWN = 5
+
+BALL_COLOR = (255, 128, 0)
+BALL_RADIUS = 10
+balls = [(90, 90)]
+
+
+def draw(screen):
+    color = (32, 64, 96)
+    pygame.draw.rect(screen, color, pygame.Rect(0, 0, WIDTH, HEIGHT))
+
+    for ball_pos in balls:
+        pygame.draw.circle(screen, BALL_COLOR, ball_pos, BALL_RADIUS)
+
+
+def handle_event(event):
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.button == LEFT_CLICK:
+            pos = pygame.mouse.get_pos()
+            balls.append(pos)
+        if event.button == RIGHT_CLICK:
+            pos = pygame.mouse.get_pos()
+            remove_el = None
+            for ball in balls:
+                diff = numpy.subtract(pos, ball)
+                if numpy.linalg.norm(diff) <= BALL_RADIUS:
+                    remove_el = ball
+                    break
+            if remove_el is not None:
+                balls.remove(remove_el)
+
+
+def main_loop():
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption('Simple physics')
+
+    done = False
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+                break
+            handle_event(event)
+
+        draw(screen)
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main_loop()
